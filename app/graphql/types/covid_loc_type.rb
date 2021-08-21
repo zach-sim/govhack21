e2008386e169534185560c157cb6a0d3f590887b
Zach Sim <zachs@datacom.co.nz>
2021-08-22 00:45:30 +1200

    Add fields to covid loc type

--- app/graphql/types/covid_loc_type.rb
+++ app/graphql/types/covid_loc_type.rb
@@ -5,6 +5,10 @@ module Types
     field :latitude, Float, null: false
     field :longitude, Float, null: false
 
+    field :alert_date, GraphQL::Types::ISO8601Date, null: false
     field :alert_details, String, null: false
+    field :alert_times, String, null: false
+    field :venue, String, null: true
+    field :site, String, null: true
   end
 end
